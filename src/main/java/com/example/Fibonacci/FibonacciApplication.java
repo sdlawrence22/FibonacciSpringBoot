6f2a9d0006fa624c3c2d9bd16d0d767b4661a642
Stephen <stevedlawrence@comcast.net>
2021-11-11 12:29:23 -0800

    Commented out print statement from book

--- src/main/java/com/example/Fibonacci/FibonacciApplication.java
+++ src/main/java/com/example/Fibonacci/FibonacciApplication.java
@@ -8,7 +8,7 @@ public class FibonacciApplication {
 
 	public static void main(String[] args) {
 		var ctx = SpringApplication.run(FibCalculator.class, args);
-		System.out.println("# Beans: " + ctx.getBeanDefinitionCount());
+		//System.out.println("# Beans: " + ctx.getBeanDefinitionCount());
 		var Fib = ctx.getBean(FibCalculator.class);
 		System.out.println(Fib);
 	}
